865db9a0549c2a7150a8769eb5b05bc65e4f8d12
Goktug Gumus <gktggumus@gmail.com>
2024-07-07 09:09:10 +0200

    Update comfyui_to_python_utils.py

--- comfyui_to_python_utils.py
+++ comfyui_to_python_utils.py
@@ -14,7 +14,7 @@ def import_custom_nodes() -> None:
     """
     import asyncio
     import execution
-    from nodes import init_custom_nodes
+    from nodes import init_builtin_extra_nodes, init_external_custom_nodes
     import server
 
     # Creating a new event loop and setting it as the default loop
@@ -26,7 +26,8 @@ def import_custom_nodes() -> None:
     execution.PromptQueue(server_instance)
 
     # Initializing custom nodes
-    init_custom_nodes()
+    init_builtin_extra_nodes()
+    init_external_custom_nodes()
 
 
 def find_path(name: str, path: str = None) -> str:
